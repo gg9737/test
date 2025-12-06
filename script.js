013bd40a5eaac5bc94a07c75e7997f36de39adcc
gg9737 <gg9737@gmail.com>
2025-12-06 21:27:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -30,6 +30,29 @@ document.getElementById("adForm").addEventListener("submit", function (e) {
   document.getElementById("thankYouContainer").classList.remove("hidden");
 });
 
+document.getElementById("adForm").addEventListener("submit", async function (e) {
+  e.preventDefault();
+
+  const formData = new FormData(this);
+
+  const response = await fetch("https://broakk72.app.n8n.cloud/webhook/f83011a2-d083-47dc-83dd-4119707f015e", {
+    method: "POST",
+    body: formData
+  });
+
+  const data = await response.json();
+
+  // Спрячь форму, покажи блок результата
+  document.getElementById("formContainer").classList.add("hidden");
+  document.getElementById("thankYouContainer").classList.remove("hidden");
+
+  // Покажи изображение
+  const imgEl = document.getElementById("resultImage");
+  imgEl.src = data.image_url;
+  imgEl.classList.remove("hidden");
+});
+
+
 // Кнопка "Заказать еще один креатив"
 function reloadPage() {
   location.reload();
